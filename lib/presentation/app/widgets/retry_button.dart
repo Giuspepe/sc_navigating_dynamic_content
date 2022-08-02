e0cb5891b77007a9e895c0738c85b84238c9c1ca
Giuseppe Cianci <giuseppe.cianci@alte-leipziger.de>
2022-08-02 08:28:28 +0200

    feat: add RetryButton, implement SearchScreen

---
+++ lib/presentation/app/widgets/retry_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RetryButton extends ConsumerWidget {
+  const RetryButton(this.provider, {super.key});
+
+  final AutoDisposeFutureProvider provider;
+
+  @override
+  Widget build(BuildContext context, ref) {
+    return ElevatedButton(
+      onPressed: () => ref.refresh(provider),
+      child: const Text(
+        'An error occured.\nRetry?',
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
--- lib/presentation/search/search_screen.dart
+++ lib/presentation/search/search_screen.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sc_navigating_dynamic_content/application/dynamic_content/dynamic_content_id_provider.dart';
 
-class SearchScreen extends StatelessWidget {
-  const SearchScreen({Key? key}) : super(key: key);
+class SearchScreen extends ConsumerWidget {
+  const SearchScreen({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Text('search screen'));
+  Widget build(BuildContext context, ref) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Search')),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter an id',
+                  ),
+                  onChanged: (value) =>
+                      ref.read(dynamicContentIdProvider.notifier).state = value,
+                  onSubmitted: (value) {
+                    if (value.isNotEmpty) {
+                      ref.read(dynamicContentIdProvider.notifier).state = value;
+                      _navigateToContent(context, value);
+                    }
+                  }),
+              Consumer(
+                builder: (context, ref, child) {
+                  final id = ref.watch(dynamicContentIdProvider);
+                  return ElevatedButton(
+                    onPressed: id.isNotEmpty
+                        ? () => _navigateToContent(context, id)
+                        : null,
+                    child: child,
+                  );
+                },
+                child: const Text('Search'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  void _navigateToContent(BuildContext context, String id) => id.isNotEmpty
+      ? context.goNamed('dynamicContent', params: {'dynamicContentId': id})
+      : null;
 }
